a8d6299f8e32b23e2ca551074fa75e8c579cfdff
Luke <luke.d.a.wilson@gmail.com>
2016-12-10 22:49:54 +0000

    Make test use stub

--- interpreter_test.py
+++ interpreter_test.py
@@ -1,6 +1,30 @@
 from time import sleep
-from robot import Robot
 from robot_algorithm import RobotAlgorithm
+
+class Sprite:
+    def rotate(self, angle):
+        print 'Rotate ' + str(angle) + ' degrees'
+        
+    def move_forward(self, direction=1): # 1=forward, -1=backwards, 0=sit on yer arse
+        print 'Move ' + ('forwards' if 1 else ('backwards' if -1 else 'nowhere'))
+        
+    def shoot(self, velocity=10):
+        print 'Shoot at ' + str(velocity) + ' velocity'
+        
+    def enemy_left(self):
+        return False
+        
+    def enemy_right(self):
+        return False
+        
+    def enemy_front(self):
+        return False
+        
+    def wall_front(self):
+        return False
+        
+    def dead(self):
+        return False
 
 input = """
 if el
@@ -18,9 +42,10 @@ if ef
     sh
 end
 fd
+sh
 """
 
-robot = Robot()
+robot = Sprite()
 algo = RobotAlgorithm(robot, input)
 
 for _ in range(20):
